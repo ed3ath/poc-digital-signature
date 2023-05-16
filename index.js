e7b844869133ae027c7c3cb19666d3776eac76e9
eD3ath <edeath1208@gmail.com>
2023-05-16 14:07:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+const server = crypto.createECDH('secp256k1');
+const client = crypto.createECDH('secp256k1');
+server.generateKeys();
+client.generateKeys();
+
+
+function signMessage(message, sharedKey) {
+    const hmac = crypto.createHmac('sha256', sharedKey);
+    hmac.update(message);
+    return hmac.digest('hex');
+}
+
+function verifySignature(message, signature, sharedKey) {
+    const hmac = crypto.createHmac('sha256', sharedKey);
+    hmac.update(message);
+    const calculatedSignature = hmac.digest('hex');
+    return signature === calculatedSignature;
+}
+
+const data = {
+    data: "hello"
+};
+
+// client sending signed message to server using client shared key
+const clientSharedKey = client.computeSecret(server.getPublicKey('hex'), 'hex');
+const signature = signMessage(JSON.stringify(data), clientSharedKey.toString('hex'));
+
+// server verifying signature using server shared key
+const serverSharedKey = server.computeSecret(client.getPublicKey('hex'), 'hex');
+console.log(verifySignature(JSON.stringify(data), signature, serverSharedKey.toString('hex')));
